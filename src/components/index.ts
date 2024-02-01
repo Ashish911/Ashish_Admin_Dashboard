c7818d5e291a45f7eb09923ee841ff4477ae024a
Ashish911 <ashishdongol32@gmail.com>
2024-02-01 19:45:58 +0000

    Some implementation commits

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,9 @@
-export { Header } from "./header";
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-charts";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+
+export { UpcomingEvents, DealsChart, UpcomingEventsSkeleton, KanbanColumnSkeleton, AccordionHeaderSkeleton, LatestActivitiesSkeleton, ProjectCardSkeleton }
